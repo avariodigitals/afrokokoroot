9fecdcdaee7611c548fbc507f72e76a003494c36
Timileyin Akinlaja <timileyin.akinlaja@vigilearn.com>
2026-03-04 19:12:48 +0100

    Fix_linting_errors

--- src/components/events/TicketPurchaseModal.tsx
+++ src/components/events/TicketPurchaseModal.tsx
@@ -68,6 +68,7 @@ export function TicketPurchaseModal({ isOpen, onClose, event }: TicketPurchaseMo
     }
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handlePayPalApprove = async (data: unknown, actions: any) => {
     setIsProcessing(true)
     try {
